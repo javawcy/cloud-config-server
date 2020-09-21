1d37079de8efc1ac82578d4b7d4870aea6aa2832
Chongyu <javawcy@outlook.com>
2020-09-21 16:07:54 +0800

    fix: change node

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ echo "====config server====="
 
 mvn clean install -Dmaven.test.skip=true
 
-docker build -f docker/dev.Dockerfile -t config-server:latest .
-docker rm -f $(docker ps -a | grep 'config-server1' | awk '{print$1}')
+docker build -f docker/dev.Dockerfile -t 192.168.10.124:8889/public/config-server:latest .
+docker rmi -f 192.168.10.124:8889/public/config-server:latest
 
-docker run -d -p 9881:8080 --name config-server1 --hostname config-server1 --network cloud-demo config-server:latest
+#docker run -d -p 9881:8080 --name config-server1 --hostname config-server1 --network cloud-demo 192.168.10.124:8889/public/config-server:latest
