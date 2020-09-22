6ef37c41adfabd118b16496f4129ce4c760f1711
Chongyu <javawcy@outlook.com>
2020-09-22 16:34:22 +0800

    fix: config change

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
+version=$1
+
 echo "====config server====="
 
 mvn clean install -Dmaven.test.skip=true
 
-docker build -f docker/dev.Dockerfile -t 192.168.10.124:8889/public/config-server:latest .
-docker push 192.168.10.124:8889/public/config-server:latest
-docker rmi -f 192.168.10.124:8889/public/config-server:latest
+docker build -f docker/dev.Dockerfile -t 192.168.10.124:8889/public/config-server:"${version}" .
+docker push 192.168.10.124:8889/public/config-server:"${version}"
+docker rmi -f 192.168.10.124:8889/public/config-server:"${version}"
 
 #docker run -d -p 9881:8080 --name config-server1 --hostname config-server1 --network cloud-demo 192.168.10.124:8889/public/config-server:latest
